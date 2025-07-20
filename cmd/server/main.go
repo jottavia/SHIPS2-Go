a55190be58ac3663dc9d8f6b46e000f9013640cd
jottavia <96543896+jottavia@users.noreply.github.com>
2025-07-20 12:50:04 -0400

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,7 +12,7 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/gin-gonic/gin"
+	"github.comcom/gin-gonic/gin"
 	"github.com/jottavia/ships-go/internal/api"
 	"github.com/jottavia/ships-go/internal/store"
 )
